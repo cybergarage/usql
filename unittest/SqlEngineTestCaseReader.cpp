a19b100b9b41e2d18314292c1befc72d3d5909e7
skonno <skonno@b45aa7d4-fa09-0410-9ebe-fd257a136781>
2011-10-30 10:16:42 +0000

    git-svn-id: svn://cybergarage.org:3960/project/sqlengine/trunk@3498 b45aa7d4-fa09-0410-9ebe-fd257a136781

---
+++ unittest/SqlEngineTestCaseReader.cpp
@@ -0,0 +1,27 @@
+/******************************************************************
+*
+* uSQL for C++
+*
+* SqlEngineTestCaseReader.cpp
+*
+* Copyright (C) Satoshi Konno 2011
+*
+* This is licensed under BSD-style license, see file COPYING.
+*
+******************************************************************/
+
+#include "SqlEngineTestCaseReader.h"
+
+SqlEngineTestCaseReader::SqlEngineTestCaseReader()
+{
+}
+
+bool SqlEngineTestCaseReader::parseFile(const char *filename)
+{
+    return false;
+}
+
+const char *SqlEngineTestCaseReader::nextCase()
+{
+    return NULL;
+}
---
+++ unittest/SqlEngineTestCaseReader.h
@@ -0,0 +1,29 @@
+/******************************************************************
+*
+* uSQL for C++
+*
+* SqlEngineTestCaseReader.h
+*
+* Copyright (C) Satoshi Konno 2011
+*
+* This is licensed under BSD-style license, see file COPYING.
+*
+******************************************************************/
+
+#ifndef CG_USQL_TESTCASEREADER_H
+#define CG_USQL_TESTCASEREADER_H
+
+#include <string>
+
+class SqlEngineTestCaseReader {
+
+public:
+
+	SqlEngineTestCaseReader();
+    
+    bool parseFile(const char *filename);
+    
+    const char *nextCase();
+};
+
+#endif

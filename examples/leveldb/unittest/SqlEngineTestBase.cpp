46f517e7de753752edd74e6561b4a70a6797a522
skonno <skonno@b45aa7d4-fa09-0410-9ebe-fd257a136781>
2012-02-14 06:55:42 +0000

    git-svn-id: svn://cybergarage.org:3960/project/sqlengine/trunk@3821 b45aa7d4-fa09-0410-9ebe-fd257a136781

---
+++ examples/leveldb/unittest/SqlEngineTestBase.cpp
@@ -0,0 +1,32 @@
+/******************************************************************
+*
+* uSQL for C++
+*
+* SQLEngineTestGQLParser.cpp
+*
+* Copyright (C) Satoshi Konno 2011
+*
+* This is licensed under BSD-style license, see file COPYING.
+*
+******************************************************************/
+
+#include <string>
+#include <vector>
+
+#include <boost/test/unit_test.hpp>
+
+#include <cybergarage/sql/SQLParser.h>
+
+using namespace std;
+using namespace uSQL;
+
+BOOST_AUTO_TEST_CASE(SQLParserStatementAddTest)
+{
+    SQLStatement sqlStmt;
+    
+    for (int n=0; n<100; n++) {
+        SQLSelect *sqlCmd = new SQLSelect();
+        sqlStmt.addChildNode(sqlCmd);
+        //BOOST_CHECK(sqlStmt.numChildNodes() == (n+1));
+    }
+}

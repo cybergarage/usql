65241e689c284b8d6d9681d3fdff9b9aa5848155
skonno <skonno@b45aa7d4-fa09-0410-9ebe-fd257a136781>
2012-02-14 06:58:02 +0000

    git-svn-id: svn://cybergarage.org:3960/project/sqlengine/trunk@3822 b45aa7d4-fa09-0410-9ebe-fd257a136781

---
+++ examples/leveldb/unittest/SqlEngineLevelDBTestMain.cpp
@@ -0,0 +1,16 @@
+/******************************************************************
+*
+* uSQL for C++
+*
+* SqlEngineTestMain.cpp
+*
+* Copyright (C) Satoshi Konno 2011
+*
+* This is licensed under BSD-style license, see file COPYING.
+*
+******************************************************************/
+
+#define BOOST_TEST_MODULE SQLEngineTest
+#define BOOST_TEST_DYN_LINK
+
+#include <boost/test/unit_test.hpp>

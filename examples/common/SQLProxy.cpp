3e15975354a53e92aec55a2b64762e923b3aa56f
skonno <skonno@b45aa7d4-fa09-0410-9ebe-fd257a136781>
2012-02-22 05:53:33 +0000

    git-svn-id: svn://cybergarage.org:3960/project/sqlengine/trunk@3839 b45aa7d4-fa09-0410-9ebe-fd257a136781

--- examples/common/SQLProxy.cpp
+++ examples/common/SQLProxy.cpp
@@ -25,6 +25,12 @@ uSQL::SQLProxy::~SQLProxy()
 
 bool uSQL::SQLProxy::getKey(SQLStatement *stmt, std::string &key, SQLError &error) 
 {
+    SQLCommand *sqlCmd = stmt->getCommandNode();
+    if (!sqlCmd) {
+        error.setMessage("COMMAND section was not found");
+        return false;
+    }
+    
     SQLFrom *sqlFrom = stmt->getFromNode();
     if (!sqlFrom) {
         error.setMessage("FROM section was not found");
--- examples/leveldb/unittest/LevelDBTestSQL.cpp
+++ examples/leveldb/unittest/LevelDBTestSQL.cpp
@@ -28,4 +28,28 @@ BOOST_AUTO_TEST_CASE(LevelDBSQLInsertTest)
     LevelDB levelDb;
     BOOST_CHECK(levelDb.open(dbFilename));
 
+    UnQLParser unqlParser;
+    
+    BOOST_CHECK(unqlParser.parse("INSERT INTO TESTDB (KEY) VALUES (1)"));
+    
+    /*
+        if (unqlParser.parse(line) == false) {
+            printf("Parser Error :  %s\n", line);
+            continue;
+        }
+        
+        SQLStatementList *stmtList = unqlParser.getStatements();
+        for (SQLStatementList::iterator stmt = stmtList->begin(); stmt != stmtList->end(); stmt++) {
+            SQLResult sqlResult;
+            if (levelDb.execSQLStatement(*stmt, sqlResult) == true) {
+                if (sqlResult.hasMessage())
+                    cout << sqlResult.getExecMessage() << endl;
+                cout << "Done." << endl;
+            }
+            else {
+                OutputSQLError(sqlResult.getErrorMessage());
+                continue;
+            }
+        }
+    */
 }
